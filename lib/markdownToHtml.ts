8085566f5dfe7560d232f3f56ea40c4bd71f6100
Juanjo Jaramillo <juan.jaramillo02@epn.edu.ec>
2021-05-24 13:42:16 -0500

    markdown to html implementation

---
+++ lib/markdownToHtml.ts
@@ -0,0 +1,13 @@
+import remark from 'remark'
+import html from 'remark-html'
+import prism from 'remark-prism'
+import oembed from 'remark-oembed'
+
+export default async function markdownToHtml (markdown: string) {
+  const result = await remark()
+    .use(oembed, { syncWidget: true, asyncImg: true })
+    .use(html)
+    .use(prism)
+    .process(markdown)
+  return result.toString()
+}
